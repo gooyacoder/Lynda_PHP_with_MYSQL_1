211e0a8ea318bc8337ab7f4fbd830839ff658591
amirhosein <gooyacoder@gmail.com>
2020-10-22 19:57:57 +0330

    Upto 012

--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -1,6 +1,10 @@
 <?php 
 	
-	$id = $_GET['id'];
+	$id = $_GET['id'] ?? '1';
 	echo $id;
 
  ?>
+
+<a href="show.php?name=<?php echo urlencode('John Doe'); ?>">Link</a><br />
+<a href="show.php?company=<?php echo urlencode('Widgets&More'); ?>">Link</a><br />
+<a href="show.php?query=<?php echo urlencode('!#*?'); ?>">Link</a><br />
